c19822819580b8313c00070c9051ee9c89971c67
B4dT0bi <tobias.boese@gmail.com>
2024-05-18 12:02:27 +0000

    fix issue with not so capable llms choosing the wrong agent name by including quotation marks

--- src/crewai/tools/agent_tools.py
+++ src/crewai/tools/agent_tools.py
@@ -1,4 +1,4 @@
-from typing import List, Union
+from typing import List
 
 from langchain.tools import StructuredTool
 from pydantic import BaseModel, Field
@@ -7,6 +7,11 @@ from crewai.agent import Agent
 from crewai.task import Task
 from crewai.utilities import I18N
 
+import logging
+
+# Konfiguration des Loggers
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger("AgentTools")
 
 class AgentTools(BaseModel):
     """Default tools around agent delegation"""
@@ -20,44 +25,38 @@ class AgentTools(BaseModel):
                 func=self.delegate_work,
                 name="Delegate work to co-worker",
                 description=self.i18n.tools("delegate_work").format(
-                    coworkers=f"[{', '.join([f'{agent.role}' for agent in self.agents])}]"
+                    coworkers=f"[{', '.join([f'{agent.role.casefold().strip()}' for agent in self.agents])}]"
                 ),
             ),
             StructuredTool.from_function(
                 func=self.ask_question,
                 name="Ask question to co-worker",
                 description=self.i18n.tools("ask_question").format(
-                    coworkers=f"[{', '.join([f'{agent.role}' for agent in self.agents])}]"
+                    coworkers=f"[{', '.join([f'{agent.role.casefold().strip()}' for agent in self.agents])}]"
                 ),
             ),
         ]
         return tools
 
-    def delegate_work(self, task: str, context: str, coworker: Union[str, None] = None, **kwargs):
+    def delegate_work(self, coworker: str, task: str, context: str):
         """Useful to delegate a specific task to a co-worker passing all necessary context and names."""
-        coworker = coworker or kwargs.get("co_worker") or kwargs.get("co-worker")
-        is_list = coworker.startswith("[") and coworker.endswith("]")
-        if is_list:
-            coworker = coworker[1:-1].split(",")[0]
         return self._execute(coworker, task, context)
 
-    def ask_question(self, question: str, context: str, coworker: Union[str, None] = None, **kwargs):
+    def ask_question(self, coworker: str, question: str, context: str):
         """Useful to ask a question, opinion or take from a co-worker passing all necessary context and names."""
-        coworker = coworker or kwargs.get("co_worker") or kwargs.get("co-worker")
-        is_list = coworker.startswith("[") and coworker.endswith("]")
-        if is_list:
-            coworker = coworker[1:-1].split(",")[0]
         return self._execute(coworker, question, context)
 
-    def _execute(self, agent, task, context):
+    def _execute(self, coworker, task, context):
         """Execute the command."""
+        logging.info(f"Searching for co-worker : [{coworker}]")
         try:
             agent = [
                 available_agent
                 for available_agent in self.agents
-                if available_agent.role.casefold().strip() == agent.casefold().strip()
+                if available_agent.role.casefold().strip() == coworker.casefold().replace('"', '').replace("'", '').strip()
             ]
-        except Exception as _:
+        except Exception as e:
+            logger.error(f"Error while filtering agents: {e}")
             return self.i18n.errors("agent_tool_unexsiting_coworker").format(
                 coworkers="\n".join(
                     [f"- {agent.role.casefold()}" for agent in self.agents]
@@ -65,16 +64,16 @@ class AgentTools(BaseModel):
             )
 
         if not agent:
+            logger.error(f"Error mentioned agent [{coworker}] could not be found")
             return self.i18n.errors("agent_tool_unexsiting_coworker").format(
                 coworkers="\n".join(
                     [f"- {agent.role.casefold()}" for agent in self.agents]
                 )
             )
 
-        agent = agent[0]
         task = Task(
             description=task,
-            agent=agent,
+            agent=agent[0],
             expected_output="Your best answer to your co-worker asking you this, accounting for the context shared.",
         )
-        return agent.execute_task(task, context)
+        return agent[0].execute_task(task, context)
